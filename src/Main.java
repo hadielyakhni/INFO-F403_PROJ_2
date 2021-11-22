cd4c81b78051abfdceabeddf2970d8decc7c2d83
hadielyakhni <hadiyakhni33@gmail.com>
2021-11-22 03:00:59 +0200

    nothing

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,9 @@ public class Main {
      * @param argv - file name to be scanned by the scanner
      */
     public static void main(String[] argv) throws Exception {
-        ScannerManager sm = new ScannerManager(argv[0]);
-
         String grammar = IO.readFile("../more/grammar.txt");
         GrammarManager gm = new GrammarManager(grammar);
+        ScannerManager sm = new ScannerManager(argv[0]);
 
         Parser parser = new Parser(gm, sm);
         parser.parse();
